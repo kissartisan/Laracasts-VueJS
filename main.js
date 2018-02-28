a9922cba63c9a76b9c85b2c08c4183c6b66fc2b8
reymark <reymark@kickstart.intranet.i4asiacorp.com>
2018-02-28 16:22:36 +0800

    Demonstrate component communication using event dispatcher

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+}
+
 Vue.component('coupon', {
     template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
 
     methods: {
         onCouponApplied() {
-            this.$emit('applied');
+            // this.$emit('applied');
+            Event.fire('applied');
         }
     }
 });
@@ -20,5 +35,9 @@ new Vue({
         onCouponAppliedInParent() {
             this.couponApplied = true;
         }
+    },
+
+    created() {
+        Event.listen('applied', () => alert('Handling coupon....'));
     }
 });
