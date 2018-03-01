c922841f67624a78f462d45c6dbf858c9a40c419
reymark <reymark@kickstart.intranet.i4asiacorp.com>
2018-03-01 10:38:10 +0800

    Setup a new project using vue/cli

---
+++ step16/my-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+      <message>Hello There</message>
+      <message>Hello Once Again</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
